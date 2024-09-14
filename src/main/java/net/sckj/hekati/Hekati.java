3cc896ec8494357e0f73eb3cd1351cd35e594c39
Jan Wersching <jan.wersching75@gmail.com>
2024-09-14 10:10:35 +0200

    added fix

--- src/main/java/net/sckj/hekati/Hekati.java
+++ src/main/java/net/sckj/hekati/Hekati.java
@@ -2,6 +2,7 @@ package net.sckj.hekati;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sckj.hekati.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,11 +12,11 @@ public class Hekati implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "hetaki";
+	public static final String MOD_ID = "hekati";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/sckj/hekati/items/ModItems.java
@@ -0,0 +1,29 @@
+package net.sckj.hekati.items;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.sckj.hekati.Hekati;
+
+public class ModItems {
+    public static final Item Ron = registerItem("ron", new Item(new Item.Settings()));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Hekati.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Hekati.LOGGER.info("Registering Mod Items for " + Hekati.MOD_ID);
+
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+           entries.add(Ron);
+        });
+
+    }
+}
