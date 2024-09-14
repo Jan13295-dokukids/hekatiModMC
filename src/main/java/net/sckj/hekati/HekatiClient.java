8ee7eda82b4676dd0ee9fe3352a209c0bdc54659
Jan Wersching <jan.wersching75@gmail.com>
2024-09-14 09:40:43 +0200

    Initial commit

---
+++ src/main/java/net/sckj/hekati/HekatiClient.java
@@ -0,0 +1,10 @@
+package net.sckj.hekati;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HekatiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
