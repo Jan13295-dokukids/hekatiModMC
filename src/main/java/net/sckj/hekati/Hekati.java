2f7b1fc43e9cf3a9b22259030ae995d20641e34c
Jan Wersching <jan.wersching75@gmail.com>
2024-09-14 15:22:44 +0200

    Added Block of Ron.

--- src/main/java/net/sckj/hekati/Hekati.java
+++ src/main/java/net/sckj/hekati/Hekati.java
@@ -2,13 +2,14 @@ package net.sckj.hekati;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.sckj.hekati.block.ModBlocks;
 import net.sckj.hekati.items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Hekati implements ModInitializer {
 
-	// test for changes.
+
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
@@ -18,5 +19,6 @@ public class Hekati implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
